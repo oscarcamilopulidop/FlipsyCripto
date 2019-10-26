61a8a464b96b7478bed21187183fcb3e778416e4
marobayos <marobayos@unal.edu.co>
2019-10-25 23:39:55 -0500

    :bug: Fixing bugs # Conflicts: #	ui/package-lock.json #	ui/src/App.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css'
-import Amplify from 'aws-amplify'
-import config from './aws-exports'
+//import Amplify from 'aws-amplify'
+//import config from './aws-exports'
 import useGlobalState from './GlobalState/useGlobalState'
 import Context from './GlobalState/context'
-import {ApolloProvider} from '@apollo/react-hooks'
 
-Amplify.configure(config)
 
-const client = new ApolloClient({
-    uri:"http://18.190.119.49:4000"
-})
-
+//Amplify.configure(config)
 
 const Index = () => {
     const store = useGlobalState()
     return (
         <Context.Provider value={store}>
-            <ApolloProvider client={client}>
-                <App />
-            </ApolloProvider>
+            <App />
         </Context.Provider>
     )
 }
