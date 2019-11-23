2eb85c3f277a88d9de0ebccbd8a7a9a8a5cf6aca
chris <camantillad@unal.edu.co>
2019-11-23 13:03:19 -0500

    add observing_users field in GET_DECKS query

--- ui/src/componentes/SearchDecks.js
+++ ui/src/componentes/SearchDecks.js
@@ -18,6 +18,9 @@ const GET_DECKS = gql`
                 title
                 idFcg
                 idUser
+                observing_users {
+                  idUser
+                }
             }
     }
 }`;
@@ -58,10 +61,7 @@ const SearchDecks = props => {
     });
 
     if (!loading) {
-        // data.filter((item,i) => {
-        //     console.log(item)
-        // })
-        console.log(typeof (data))
+        console.log(data)
     }
 
     const addDeck = idFcg => {
